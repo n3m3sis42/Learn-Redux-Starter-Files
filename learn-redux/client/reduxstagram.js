0520a54ce4adfcff47e852344046bd3195cad28f
n3m3sis42 <n3m3sis42@gmail.com>
2017-08-31 19:44:31 -0400

    built Main component -- tutorial used old version of React Router, but I used current version instead

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,9 @@
-// let's go!
+import React from 'react'
+import {render} from 'react-dom'
+import css from './styles/style.styl'
+import Main from './components/Main'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+render(
+  <Router><Main /></Router>, document.getElementById('root')
+);
