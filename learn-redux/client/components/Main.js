8eea12e03bb757f53ceb019e63c49a976a84da3d
n3m3sis42 <n3m3sis42@gmail.com>
2017-08-31 20:32:55 -0400

    built Single and PhotoGrid components and added routes to them -- tutorial used old version of React Router, but I used current version instead

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Link, Route, Switch } from 'react-router-dom'
+import Single from './Single'
+import PhotoGrid from './PhotoGrid'
 
 export default class Main extends Component {
 
@@ -7,6 +9,18 @@ export default class Main extends Component {
     return (
       <div>
         <Link to="/"><h1>Reduxstagram</h1></Link>
+        <Switch>
+          <Route exact path='/' render={(props) => {
+            return <PhotoGrid
+                history={props.history}
+              />}
+          } />        
+          <Route exact path='/view/:postId' render={(props) => {
+            return <Single
+                history={props.history}
+              />}
+          } />
+        </Switch>
       </div>
     )
   }
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Single extends Component {
+
+  render() {
+    return (
+      <div className="single-photo">
+        Single
+      </div>
+    )
+  }
+
+}
